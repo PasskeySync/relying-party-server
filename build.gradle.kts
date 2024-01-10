720000492578581fc995c8203555aa3f36099b24
LittleEtx <1968717506@qq.com>
2024-01-11 01:55:00 +0800

    使用了配置文件来启动；更新了引用库

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val webauthn_server_version: String by project
-val webauthn4j_version: String by project
-val ehcache_version: String by project
+val ktorVersion: String by project
+val kotlinVersion: String by project
+val logbackVersion: String by project
+val webauthnServerVersion: String by project
+val ehcacheVersion: String by project
+val exposedVersion: String by project
+val h2Version: String by project
+
 
 plugins {
     kotlin("jvm") version "1.9.21"
@@ -30,17 +32,20 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-sessions-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
-    implementation("org.ehcache:ehcache:$ehcache_version") // caching
+    implementation("org.ehcache:ehcache:$ehcacheVersion") // caching
 
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("com.yubico:webauthn-server-core:$webauthn_server_version")
-    implementation("org.projectlombok:lombok:1.18.30")
-    implementation("com.webauthn4j:webauthn4j-core:$webauthn4j_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion") // database
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("com.h2database:h2:$h2Version")
 
-    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("com.yubico:webauthn-server-core:$webauthnServerVersion")
+
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktorVersion")
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
--- src/main/kotlin/com/littleetx/Application.kt
+++ src/main/kotlin/com/littleetx/Application.kt
@@ -5,15 +5,9 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main() {
-    embeddedServer(Netty,
-        port = 8080,
-        host = "0.0.0.0",
-        module = Application::module,
-        watchPaths = listOf("classes")
-    ).start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
+@Suppress("unused") // Referenced in application.conf
 fun Application.module() {
     configureSecurity()
     configureRouting()
